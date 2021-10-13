4d3afd567ec9090884f61b4c26256bef1dd41b08
elliott kung <elliott7506@rogers.com>
2021-10-13 15:46:32 -0400

    initial setup of authentication for the spotify demo app with web view to the login page for spotify and getting response code returned.

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -10,10 +10,24 @@ import Foundation
 final class AuthManager{
     static let shared = AuthManager()
     
+    struct Constants{
+        static let clientID = "3b8a6613d348457ea3ca779d37e83136"
+        static let clientSecret =  "2ba2e20370e943799f875783f03e4946"
+    }
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        
+        return URL(string: string)
+    }
+    
     private init(){}
     
     var isSignedIn: Bool {
-        return true
+        return false
     }
     
     private var accessToken: String?{
@@ -30,6 +44,22 @@ final class AuthManager{
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    ){
+        // get token
+        
+    }
+    
+    public func refreshAccessToken(){
+        
+    }
+    
+    private func cacheToken(){
+        
     }
     
 }

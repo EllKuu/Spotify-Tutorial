51cd635845d449a8d065f7e89051fe10d215f83f
elliott kung <elliott7506@rogers.com>
2021-11-06 18:10:44 -0400

    added the albumVC functionality to show album details and tracks.

--- Spotify/Resources/Extensions.swift
+++ Spotify/Resources/Extensions.swift
@@ -35,3 +35,27 @@ extension UIView{
     }
     
 }
+
+extension DateFormatter{
+    // creating dateformatter static as they are exspensive for memory to be made over and over so we creating it once
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd"
+        return dateFormatter
+    }()
+    
+    static let displayDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        return dateFormatter
+    }()
+}
+
+extension String{
+    static func formattedDate(string: String) -> String{
+        guard let date = DateFormatter.dateFormatter.date(from: string) else {
+            return string
+        }
+        return DateFormatter.displayDateFormatter.string(from: date)
+    }
+}
---
+++ Spotify/ViewModels/AlbumCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCollectionViewCellViewModel.swift
+//  Spotify
+//
+//  Created by elliott kung on 2021-11-06.
+//
+
+import Foundation
+
+struct AlbumCollectionViewCellViewModel{
+    let name: String
+    let artistName: String
+}

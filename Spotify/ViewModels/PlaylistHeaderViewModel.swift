5e26cab05f12e198b0570b86ce37945003e055be
elliott kung <elliott7506@rogers.com>
2021-11-06 17:00:44 -0400

    added playlist VC and configured the layout to show playlist details in reusaable headerview. Also implmented play all button and share feature for the VC.

---
+++ Spotify/ViewModels/PlaylistHeaderViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaylistHeaderViewModel.swift
+//  Spotify
+//
+//  Created by elliott kung on 2021-11-06.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewModel{
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}

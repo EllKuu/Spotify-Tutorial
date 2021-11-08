fea50f2735ea1f126d32525ffc507057e9a0b66b
elliott kung <elliott7506@rogers.com>
2021-11-08 18:19:45 -0500

    added the layout for the player view controller and setup the controls for the audio player.

---
+++ Spotify/Presenter/PlaybackPresenter.swift
@@ -0,0 +1,26 @@
+//
+//  PlaybackPresenter.swift
+//  Spotify
+//
+//  Created by elliott kung on 2021-11-08.
+//
+
+import Foundation
+import UIKit
+
+final class PlaybackPresenter {
+    
+    static func startPlayback(from viewController: UIViewController, track: AudioTrack){
+        let vc = PlayerViewController()
+        vc.title = track.name
+        viewController.present(UINavigationController(rootViewController: vc), animated: true, completion: nil)
+        
+    }
+    
+    static func startPlayback(from viewController: UIViewController, tracks: [AudioTrack]){
+        let vc = PlayerViewController()
+        viewController.present(UINavigationController(rootViewController: vc), animated: true, completion: nil)
+    }
+    
+    
+}
